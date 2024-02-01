2e18fd90ce8a5d1730f53b0de05369a6d01a7ccc
devanand-jrx <devanand.ravindran@edstem.com>
2024-02-01 13:12:24 +0530

    service test updated

--- src/test/java/com/edstem/employeemanagementsystem/controller/EmployeeControllerTest.java
+++ src/test/java/com/edstem/employeemanagementsystem/controller/EmployeeControllerTest.java
@@ -3,6 +3,7 @@ package com.edstem.employeemanagementsystem.controller;
 
 import com.edstem.employeemanagementsystem.contract.request.EmployeeRequest;
 import com.edstem.employeemanagementsystem.contract.response.EmployeeResponse;
+import com.edstem.employeemanagementsystem.model.Employee;
 import com.edstem.employeemanagementsystem.service.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,16 +37,43 @@ public class EmployeeControllerTest {
 
     @Test
     void testCreateEmployee() throws Exception {
-        EmployeeRequest request = new EmployeeRequest("name","dev@gmail.com","Dev");
+        Long id = 1L;
+        EmployeeRequest request = new EmployeeRequest("dev","dev@gmail.com","Dev");
         EmployeeResponse expectedResponse = new EmployeeResponse(1L, "dev","dev@gmail.com","Dev");
         when(employeeService.createEmployee(request)).thenReturn(expectedResponse);
         mockMvc.perform(
                 post("/employees")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(request)))
+                .andDo(print())
                 .andExpect(status().isOk())
-                        .andExpect(content().json(new ObjectMapper().writeValueAsString(expectedResponse)));
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(expectedResponse)));
     }
+
+    @Test
+    void testGetEmployeeByDepartment() throws Exception {
+        String department = "BDA";
+        List<EmployeeResponse> employeeResponses = new ArrayList<>();
+        employeeResponses.add(new EmployeeResponse(1L, "Devanand", "devanand@gmail.com", "BDA"));
+        employeeResponses.add(new EmployeeResponse(2L, "sharuk", "sharuk@gmail.com", "BDA"));
+        when(employeeService.getEmployeeByDepartment(department)).thenReturn(employeeResponses);
+
+        mockMvc.perform(get("/employees/department/" + department))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(employeeResponses)));
+
+
+
+    }
+
+//    @Test
+//
+//    void testGetEmployee() throws Exception{
+//        Long id = 1L;
+//        EmployeeResponse employeeResponse = new EmployeeResponse(1L, "Devanand", "devanand@gmail.com", "BDA");
+
+
 
 }
 
--- src/test/java/com/edstem/employeemanagementsystem/service/EmployeeServiceTest.java
+++ src/test/java/com/edstem/employeemanagementsystem/service/EmployeeServiceTest.java
@@ -10,13 +10,17 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class EmployeeServiceTest {
     private EmployeeRepository employeeRepository;
-
     private ModelMapper modelMapper;
     private EmployeeService employeeService;
 
@@ -35,10 +39,37 @@ public class EmployeeServiceTest {
         Employee employee = modelMapper.map(employeeRequest, Employee.class);
         EmployeeResponse expectedResponse = modelMapper.map(employee, EmployeeResponse.class);
         when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
-        when(modelMapper.map(employee, EmployeeResponse.class)).thenReturn(expectedResponse);
         EmployeeResponse actualResponse = employeeService.createEmployee(employeeRequest);
         assertEquals(expectedResponse, actualResponse);
     }
+
+    @Test
+    void testGetEmployee(){
+        Long id = 1L;
+        Employee employee = new Employee(id, "Devanand", "devanand@gmail.com", "BDA");
+        EmployeeResponse expectedResponse = modelMapper.map(employee, EmployeeResponse.class);
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(employee));
+        EmployeeResponse actualResponse = employeeService.getEmployee(id);
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+    @Test
+    void testGetEmployeeByDepartment(){
+        String department = "BDA";
+
+        Employee employee1 = new Employee(1L, "Devanand", "devanand@gmail.com", "BDA");
+        Employee employee2 = new Employee(2L, "Sharok", "sharok@gmail.com", "BDA");
+        List<Employee> employees = Arrays.asList(employee1, employee2);
+        List<EmployeeResponse> expectedResponse = employees.stream().map(employee -> modelMapper.map(employee, EmployeeResponse.class))
+                .collect(Collectors.toList());
+        when(employeeRepository.findByDepartment(department)).thenReturn(employees);
+        List<EmployeeResponse> actualResponse = employeeService.getEmployeeByDepartment(department);
+
+
+        assertEquals(expectedResponse, actualResponse);
+
+    }
+
 
 
 }
